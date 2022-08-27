d46bb701a5802a94ca497cebd62a9439c02e7fc0
= <ianbour2016@gmail.com>
2022-08-27 03:25:23 +0200

    🎉 Initial commit

---
+++ client.lua
@@ -0,0 +1,15 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local playerData = QBCore.Functions.GetPlayerData()
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000)
+        local ped = PlayerPedId()
+        if IsPedInAnyVehicle(ped, false) then
+            SetUserRadioControlEnabled(false)
+            if GetPlayerRadioStationName() ~= nil then
+                SetVehRadioStation(GetVehiclePedIsIn(PlayerPedId()),"OFF")
+            end
+        end
+    end
+end)
