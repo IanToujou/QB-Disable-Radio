85b5002900e306c2a251f7ec5df1269e5e69df96
= <ianbour2016@gmail.com>
2022-08-27 19:37:18 +0200

    📦 Updated manifest and file structure

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'IanToujou'
+description 'A script to disable the default GTA V radio.'
+version '1.0.1'
+
+client_scripts {
+    'client/main.lua',
+}
+
+lua54 'yes'
